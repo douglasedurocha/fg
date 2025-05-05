2da9de0dd296fe8561041c0ebdafae474c6d1ee1
allanindex <bispoa486@gmail.com>
2025-05-04 22:01:24 -0300

    upload python aplication

---
+++ fhirguard-python/cli/gui.py
@@ -0,0 +1,2 @@
+def run():
+    print('Comando gui chamado!') 
---
+++ fhirguard-python/cli/logs.py
@@ -0,0 +1,2 @@
+def run(pid, tail=None, follow=False):
+    print(f'Comando logs chamado para o PID {pid}, tail={tail}, follow={follow}!') 
